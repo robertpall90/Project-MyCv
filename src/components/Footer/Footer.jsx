435ad9c145c3800ee7b6525f7b1fd98d3a389e86
robertpall90 <pallroberto90@gmail.com>
2021-01-10 08:47:04 +0200

    chang footer icon src

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './Footer.css';
 import logo from '../../assets/reactlogo.png';
 import logoGit from "../../assets/logoGitHub.png"
+import logoMail from "../../assets/logoMail.png"
+import logoPhone from "../../assets/logoPhone.png"
 
 
 function Footer() {
@@ -10,10 +12,10 @@ function Footer() {
             <div className="divLeft">
                 <div className="contactDiv">
                     <a href="tel:+0474872227">
-                        <img alt="telephoneIcon" className="contact" src="https://www.freeiconspng.com/thumbs/phone-icon/phone-icon-clip-art--royalty--7.png"></img>
+                        <img alt="telephoneIcon" className="contact" src={logoPhone}></img>
                     </a>
                     <a href="mailto:pallroberto90@gmail.com">
-                        <img alt="mailIcon" className="contact" src="https://assets.stickpng.com/images/584856a7e0bb315b0f7675aa.png"></img>
+                        <img alt="mailIcon" className="contact" src={logoMail}></img>
                     </a>
                     <a href="https://github.com/robertpall90" target="_blank" rel="noreferrer">
                         <img alt="gitHubIcon" className="contact" src={logoGit}></img>
